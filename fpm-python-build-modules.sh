5bdfec98c3b52c04f42fa9dd21b4cb09c7e9dc45
plangl <plangl@acxiom.com>
2018-07-28 15:09:16 -0500

    adding useful scripts

---
+++ fpm-python-build-modules.sh
@@ -0,0 +1,35 @@
+if [[ $PYTHON_VERSION == "2.7" ]] ; then
+
+##package python lib using 2.7 version
+fpm -s python --python-bin /opt/python$PYTHON_VERSION/bin/python$PYTHON_VERSION --python-easyinstall /opt/python$PYTHON_VERSION/bin/easy_install-$PYTHON_VERSION -t rpm --python-package-name-prefix custom-python-$PYTHON_VERSION $PYTHON_LIB
+
+elif [[ $PYTHON_VERSION == "3.4" ]] ; then
+
+##package python lib using 3.4 version
+fpm -s python --python-bin /opt/python$PYTHON_VERSION/bin/python$PYTHON_VERSION --python-easyinstall /opt/python$PYTHON_VERSION/bin/easy_install-$PYTHON_VERSION -t rpm --python-package-name-prefix custom-python-$PYTHON_VERSION $PYTHON_LIB
+
+elif [[ $PYTHON_VERSION == "3.5" ]] ; then
+
+##package python lib using 3.5 version
+fpm -s python --python-bin /opt/python$PYTHON_VERSION/bin/python$PYTHON_VERSION --python-easyinstall /opt/python$PYTHON_VERSION/bin/easy_install-$PYTHON_VERSION -t rpm --python-package-name-prefix custom-python-$PYTHON_VERSION $PYTHON_LIB
+fi
+
+##gathering dependencies for main python lib.  Packaging those as well!!!
+
+rpm -qpR *.rpm | grep acx |  cut -f1 -d ' ' > list.out
+rpm -qpR *.rpm > dep.out
+listOfDeps=`cat list.out | cut -d '-' -f 4-`
+
+if [ -z "$listOfDeps" ];then
+
+echo "no deps for $PYTHON_LIB"
+
+else
+
+for depName in $listOfDeps
+do 
+  echo $depName
+  fpm -s python --python-bin /opt/python$PYTHON_VERSION/bin/python$PYTHON_VERSION --python-easyinstall /opt/python$PYTHON_VERSION/bin/easy_install-$PYTHON_VERSION -t rpm --python-package-name-prefix custom-python-$PYTHON_VERSION $depName 
+done
+
+fi
